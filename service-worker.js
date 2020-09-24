8ebe3244bae8bb164e65ec029f392a5a7b31bb6a
Dominik Deimel <domi.deimel@yahoo.de>
2020-09-24 23:59:12 +0200

    add

--- service-worker.js
+++ service-worker.js
@@ -2,9 +2,10 @@ const CACHE_NAME = 'DQIG_CACHE';
 const toCache = [
     '/',
     '/js/main.min.js',
+    '/css/styles.min.css',
     '/js/pwa.webmanifest',
     '/images/apple-touch.png',
-    'images/splash-screen.png'
+    '/images/splash-screen.png'
 ];
 
 self.addEventListener('install', function(event) {
